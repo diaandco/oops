04dc397be56b157aec177a5a0bbbd1617cd8021d
Zachary Friedman <zac@dia.co>
2017-10-27 16:13:55 -0400

    Revert "Ignore tmp directory for zip deploy"
    This reverts commit d3512b762cd41c92e39c4cdf15ddb3c4068dbaa0.

--- lib/oops/tasks.rb
+++ lib/oops/tasks.rb
@@ -11,7 +11,7 @@ module Oops
         prerequisites: ['assets:clean', 'assets:precompile'],
         additional_paths: [],
         includes: ['public/assets', 'public/packs'],
-        excludes: ['.gitignore', 'tmp'],
+        excludes: ['.gitignore'],
         format: 'zip'
       }
     end
